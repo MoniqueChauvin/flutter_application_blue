b0751dbf520cc02fa60dbb383bf11edd7527ac64
Monique Chauvin <monique-chauvin@hotmail.com>
2021-09-17 14:01:17 -0300

    Tela de login

--- lib/features/Login/Loginpage.dart
+++ lib/features/Login/Loginpage.dart
@@ -12,44 +12,54 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                    labelText: 'Login',
-                    border: OutlineInputBorder(),
+        appBar: AppBar(),
+        body: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/blue_projects.jpg',
+                          fit: BoxFit.contain,
+                          width: 100,
+                          height: 100,
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          textInputAction: TextInputAction.next,
+                          decoration: InputDecoration(
+                            labelText: 'Login',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          textInputAction: TextInputAction.done,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Senha',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        ElevatedButton(onPressed: () {}, child: Text('Login'))
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                TextField(
-                  textInputAction: TextInputAction.done,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-              ],
-            ),
-          ),
-        ElevatedButton(
-          onPressed: (){}, 
-          child: Text( 'Login'))
-        ],
-      ),
-    );
+                ],
+              ),
+            )));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,12 @@ import 'package:flutter/material.dart';
 import 'features/Login/Loginpage.dart';
 
 main() {
-  runApp(LoginPage());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LoginPage());
+  }
 }
